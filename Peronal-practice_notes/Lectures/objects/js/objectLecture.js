ce9b93766649562a954a258c94466fd3fd12d9bb
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-06-07 14:59:20 -0500

    Adding object lecture practice

---
+++ Peronal-practice_notes/Lectures/objects/js/objectLecture.js
@@ -0,0 +1,78 @@
+"use strict";
+console.log(`hello from objectLecture.js`);
+
+// ============================================================================================================
+// const car = {
+//     make: "Ford",
+//     model: "F-250"
+// };
+//
+// car.color = "Red";
+//
+// console.log(car);
+// console.log(car.model);
+// console.log(car["make"]); // same thing as above!
+//
+// /*
+// Make sure properties are lowercase
+// values can be uppercase
+//  */
+// ============================================================================================================
+// const addTwoNumbers = (x, y) => {return x + y};
+//
+// const object1 = {
+//     name: "Cesar",
+//     age: 31,
+//     school: "Codeup",
+//     math: addTwoNumbers(2, 2)
+// };
+//
+// console.log(object1.name);
+// console.log(object1.age);
+// console.log(object1.school);
+// console.log(object1.math);
+//
+// console.log(object1);
+//
+// object1.favoriteColor = "Green";
+// console.log(object1);
+//
+// object1.family = {};
+// console.log(object1);
+// object1.family.wife = "Laura";
+// object1.family.daughter = "Sofia";
+// object1.family.son = "Raleigh";
+// console.log(object1);
+
+// ============================================================================================================
+
+const cars = [
+    {
+        make: "Toyota",
+        model: "Camry",
+        features: ["Automatic Windows", "Bluetooth Connectivity", "GPS Navigation"],
+        owner: {
+            name: "Bob",
+            age: 30
+        },
+        honk: function(){
+            return `${this.model} honked...`;
+        }
+        // or
+        /*
+        honk() {
+            return `${this.model} honked...`;
+        }
+         */
+    }
+];
+
+console.log(cars[0].make);
+console.log(cars[0].model);
+console.log(cars[0].features[1]);
+console.log(cars[0].owner.name);
+console.log(cars[0].honk());
+
+// this keyword refers only to its parent!!!
+
+
