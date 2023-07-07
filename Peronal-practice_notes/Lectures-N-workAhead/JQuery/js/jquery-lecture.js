d2a2c8525473fb2d04abfb675609d02940fc3b65
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-07 13:59:36 -0500

    jQuery lecture

---
+++ Peronal-practice_notes/Lectures-N-workAhead/JQuery/js/jquery-lecture.js
@@ -0,0 +1,15 @@
+
+// // JS functino to only alert as the page has finished loading.
+// window.onload = function () {
+//     alert("The window is loaded");
+// };
+
+// Same thing but with jQuery:
+// jQuery(function (){
+//     alert("The window is loaded");
+// });
+
+// or use $ , which is the equivalent of writing jQuery
+$(function () {
+    alert("the window is loaded");
+});
