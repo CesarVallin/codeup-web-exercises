e4a843094a4b3176514ff4083c615176afcea277
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-10 20:38:57 -0500

    worked on hackerrank problems

---
+++ Hackerrank/alice-n-bob/js/alice-n-bob.js
@@ -0,0 +1,25 @@
+"use strict";
+console.log(`hello from alice-n-bob.js`);
+
+let alice1 = [17, 28, 30];
+let bob1 = [99, 16, 8];
+console.log(alice1);
+console.log(bob1);
+console.log(`expected output :`, [2, 1]);
+function compareTriplets(a, b) {
+    let aliceTotal = 0;
+    let bobTotal = 0;
+    for (let i = 0; i < a.length; i++) {
+        console.log(i, a[i], `alice`);
+        console.log(i, b[i], `bob`);
+        if (a[i] > b[i]) {
+            aliceTotal += 1;
+        }
+        if (a[i] < b[i]) {
+            bobTotal += 1;
+        }
+    }
+    return [aliceTotal, bobTotal];
+}
+
+console.log(compareTriplets(alice1, bob1));
---
+++ Hackerrank/factorials/js/factorials.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log(`hello from factorials.js`);
+
+function extraLongFactorials(n) {
+    let counter = BigInt(1);
+    for (let i = BigInt(n); i > 0; i--) {
+        counter *= i;
+    }
+    console.log(counter);
+    return counter;
+}
+
+console.log(extraLongFactorials(25));
+console.log(`expected: `, BigInt(15511210043330985984000000));
+console.log(`expected: `, 15511210043330985984000000n);
+
