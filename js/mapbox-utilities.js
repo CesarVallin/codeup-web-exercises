cce36a26b2ac31bf180804fd9a437911dc79a943
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-13 21:16:35 -0500

    mapbox_maps_api exercise

---
+++ js/mapbox-utilities.js
@@ -0,0 +1,73 @@
+// MAPBOX-UTILITIES
+export function initializeMap () {
+    mapboxgl.accessToken = MAPBOX_TOKEN;
+
+    const mapOptions = {
+        container: 'map',
+        style: 'mapbox://styles/mapbox/satellite-streets-v12',
+        zoom: 5,
+        center: [-103.349609, 20.659698],
+    }
+    return new mapboxgl.Map(mapOptions);
+}
+// -------------------------------------------------------------------------------------------------
+// ZOOM FUNCTIONS:
+
+// Function to zoom in:
+export function zoomIn(map) { // need to pass map as parameter
+    // console.log(`zoomIn clicked`);
+    let currentZoom = map.getZoom();
+    // console.log(currentZoom);
+    map.setZoom(++currentZoom);
+}
+// -----------------------------------
+
+// Function for zoom out:
+export function zoomOut(map) { // need to pass map as parameter
+    let currentZoom = map.getZoom();
+    map.setZoom(--currentZoom);
+}
+// --------------------------------------
+
+// Function reset zoom:
+export function resetZoom(map) { // need to pass map as parameter
+    map.setZoom(5);
+}
+// -------------------------------------------------------------------------------------------------
+// Function to center and zoom on El Parian (restaurant)
+export function elParianRest (map) { // need to pass map as parameter
+    geocode('C. Juárez 68, Centro, 45500 San Pedro Tlaquepaque, Jal., Mexico', MAPBOX_TOKEN).then((data) => {
+        console.log(data);
+        map.setCenter(data);
+        map.setZoom(17);
+    });
+}
+// --------------------------------------
+// Function to center and zoom on Cooper's BBQ
+export function coopersBBQ (map) {
+    geocode('604 W Young St, Llano, TX 78643, United States', MAPBOX_TOKEN).then((data) => {
+        console.log(data);
+        map.setCenter(data);
+        map.setZoom(17);
+    });
+}
+
+// --------------------------------------
+// Function to center and zoom on Mattenga's Pizzeria
+export function mattengasPizza (map) {
+    geocode('6044 FM3009 Ste 290, Schertz, TX 78154, United States', MAPBOX_TOKEN).then((data) => {
+        console.log(data);
+        map.setCenter(data);
+        map.setZoom(17);
+    });
+}
+
+// ------------------------------------------------------------------------------------------------
+// Functions to create markers and popups
+// Ajax request -- With JS // 'getter function'
+// request to map-restaurants.json
+export function getRestaurants () {
+    $.ajax(`data/map-restaurants.json`).done((restaurants) => {
+        console.log(restaurants);
+    });
+}
---
+++ js/mapbox_maps_api.js
@@ -0,0 +1,54 @@
+import {
+    initializeMap,
+    zoomIn,
+    zoomOut,
+    resetZoom,
+    elParianRest,
+    getRestaurants,
+    coopersBBQ, mattengasPizza
+} from './mapbox-utilities.js'
+
+(() => {
+    // Variables...
+    const map = initializeMap(); // declaring what map is!!
+// Zoom btns:
+    const zoomInBtn = document.querySelector('#zoom-in');
+    const zoomOutBtn = document.querySelector('#zoom-out');
+    const zoomResetBtn = document.querySelector('#zoom-reset');
+// Restaurant btns:
+    const elParianBtn = document.querySelector('#el-parian');
+    const coopersBtn = document.querySelector('#coopers');
+    const mattengasBtn = document.querySelector('#mattengas');
+    // -------------------------------------------------------------------------------------------------
+    // Events:
+// Zoom events:
+    zoomInBtn.addEventListener('click', ()=>{
+        zoomIn(map); // need to pass map as parameter
+    });
+
+    zoomOutBtn.addEventListener('click', ()=>{
+        zoomOut(map); // need to pass map as parameter
+    });
+
+    zoomResetBtn.addEventListener('click', () => {
+        resetZoom(map); // need to pass map as parameter
+    });
+
+// Restaurant events:
+    elParianBtn.addEventListener('click', () => {
+        elParianRest(map); // need to pass map as parameter
+    });
+
+    coopersBtn.addEventListener('click', () => {
+        coopersBBQ(map);
+    });
+
+    mattengasBtn.addEventListener('click', () => {
+        mattengasPizza(map);
+    })
+
+    // -------------------------------------------------------------------------------------------------
+
+    // Function imported from mapbox-utilities.js
+    getRestaurants();
+})();
