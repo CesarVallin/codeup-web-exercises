61121e8085874f55e5f1de74bb4f92ca1841cf87
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-17 19:18:20 -0500

    working on codewars problems

--- Peronal-practice_notes/A-blank-space-for-practice/js/A-blank-space-for-practice.js
+++ Peronal-practice_notes/A-blank-space-for-practice/js/A-blank-space-for-practice.js
@@ -5,7 +5,16 @@ console.log(`hello from A-blank-space-for-practice.js, external JS`);
 // ========================================================================================================
 
 
+function validatePIN (pin) {
+    console.log(pin);
+    let pattern = /[0-9]/g;
+    console.log(pattern.length);
+    let pattern1 = /[-]/g;
+    console.log(pattern1.length);
 
+}
+
+console.log(validatePIN("-1234"));
 
 
 
