0eed9f411aba8506b85d9a8b37e1f621a0c0dc25
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-20 16:09:11 -0500

    map filter and reduce worked as variables as well

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -39,7 +39,7 @@ const users = [
     }
 ];
 
-
+// ------------------------------------------------------------------------------------------------------------
 // 2 Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 function atLeast3Languages(arrParam) {
     const filterAtLeast3Langs = arrParam.filter(user => {
@@ -48,7 +48,12 @@ function atLeast3Languages(arrParam) {
     return filterAtLeast3Langs;
 }
 console.log(atLeast3Languages(users));
-
+// Outside of a function equivalent..
+const threeLangs = users.filter(user => {
+    return user.languages.length > 2;
+});
+console.log(threeLangs);
+// ------------------------------------------------------------------------------------------------------------
 // 3 Use .map to create an array of strings where each element is a user's email address
 function arrOfUsersEmails(arrParam) {
     const usersEmails = arrParam.map(user => {
@@ -57,16 +62,30 @@ function arrOfUsersEmails(arrParam) {
     return usersEmails;
 }
 console.log(arrOfUsersEmails(users));
-
+// Outside of a function equivalent..
+const userEmailArr = users.map(user => {
+    return user.email;
+});
+console.log(userEmailArr);
+// ------------------------------------------------------------------------------------------------------------
 // 4 Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
-function totalYearsExperience(arrParam) {
+function avgYearsExperience(arrParam) {
     const total = arrParam.reduce((accumulator, currentVal) => {
         return accumulator + currentVal.yearsOfExperience;
     }, 0);
-    return total;
+    return total / arrParam.length;
 }
-console.log(totalYearsExperience(users));
-
+console.log(avgYearsExperience(users));
+// Outside of a function equivalent..
+const avgExperience = users.reduce((accumulator, user, index) => {
+    accumulator += user.yearsOfExperience;
+    if ( (index + 1) === users.length ) {
+        accumulator = accumulator / users.length;
+    }
+    return accumulator;
+}, 0);
+console.log(avgExperience);
+// ------------------------------------------------------------------------------------------------------------
 // 5 Use .reduce to get the longest email from the list of users.
 function getLongestEmail (arrParam) {
     const longestEmail = arrParam.reduce((accumulator, user) => {
@@ -79,7 +98,14 @@ function getLongestEmail (arrParam) {
     return longestEmail;
 }
 console.log(getLongestEmail(users));
-
+const longestEmail = users.reduce((accumulator, user) => {
+    if (user.email.length > accumulator.length) {
+        accumulator = user.email;
+    }
+    return accumulator;
+}, '');
+console.log(longestEmail);
+// ------------------------------------------------------------------------------------------------------------
 // 6 Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 function getListOfUsernamesAsString(arrParam) {
     let userNameString = arrParam.reduce((accumulator, person) => {
@@ -94,7 +120,16 @@ function getListOfUsernamesAsString(arrParam) {
     return userNameString;
 }
 console.log(getListOfUsernamesAsString(users));
-
+const usersListString = users.reduce((accumulator, user) => {
+    if (user === users[users.length - 1]) {
+        accumulator += `and ${user.name}.`
+    } else {
+        accumulator += `${user.name}, `
+    }
+    return accumulator;
+}, "Your instructors are: ");
+console.log(usersListString);
+// ------------------------------------------------------------------------------------------------------------
 // BONUS Use .reduce to get the unique list of languages from the list of users.
 function getUniqueListofLanguages(arrParam) {
     const uniqueLanguageList = arrParam.reduce((accumulator, user) => {
