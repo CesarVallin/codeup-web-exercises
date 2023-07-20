7c179acc51c7ae56d505bcf80170c1eca9289afc
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-20 13:29:38 -0500

    map filter reduce exercises done

--- js/map-filter-reduce-lessons.js
+++ js/map-filter-reduce-lessons.js
@@ -28,6 +28,11 @@ console.log(`hello from map-filter-reduce-lessons`);
             // }
             // console.log(housesTwoBedsPlus);
             // ------------------------------------------------------------------------------------------
+            // Filter Method
+            /*
+            When do you use filter?
+            WHEN YOU WANT TO FILTER OUT ELEMENTS FROM AN ARRAY BASED ON A CONDITION
+            //  */
             let housesTwoBedsPlus = houses.filter(house => {
                 // const nmrOfBeds = parseFloat(house.beds); // You could have also passed this variable below
                 return parseFloat(house.beds) >= 5;
@@ -36,7 +41,26 @@ console.log(`hello from map-filter-reduce-lessons`);
                 const nmrOfBaths = parseFloat(house.baths);
                 return nmrOfBaths >= 2;
             })
-            console.log(housesTwoBedsPlus);
+            console.log(`Filtered houses: `,housesTwoBedsPlus);
+            // ------------------------------------------------------------------------------------------
+            // Map Method
+            /*
+            When do you use a map?
+            WHEN YOU WANT TO TRANSFORM EACH ELEMENT IN AN ARRAY AND RETURN A NEW ARRAY WITH THE TRANSFORMED ELEMENTS
+             */
+            let housesPriceRange = houses.map(house => {
+                const priceRange = 200_000;
+                const canBuy = parseFloat(house.price) > priceRange ? 'Nope' : 'Yes!';
+                const newValue = `Address: ${house.address} \nCan I afford it: ${canBuy}`;
+                return newValue;
+            });
+            for (let house of housesPriceRange) {
+                console.log(house);
+            }
+
+            // ------------------------------------------------------------------------------------------
+
+
         });
 
 
--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -47,10 +47,51 @@ function atLeast3Languages(arrParam) {
     });
     return filterAtLeast3Langs;
 }
-
 console.log(atLeast3Languages(users));
 
 // 3 Use .map to create an array of strings where each element is a user's email address
-function arrOfUsersEmails() {
-
+function arrOfUsersEmails(arrParam) {
+    const usersEmails = arrParam.map(person => {
+        return person.email;
+    });
+    return usersEmails;
 }
+console.log(arrOfUsersEmails(users));
+
+// 4 Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+function totalYearsExperience(arrParam) {
+    const total = arrParam.reduce((accum, currentVal) => {
+        return accum + currentVal.yearsOfExperience;
+    }, 0);
+    return total;
+}
+console.log(totalYearsExperience(users));
+
+// 5 Use .reduce to get the longest email from the list of users.
+function getLongestEmail (arrParam) {
+    const longestEmail = arrParam.reduce((champ, user) => {
+        console.log(champ, user);
+        if (user.email.length > champ.length) {
+            champ = user.email
+        }
+        return champ;
+    }, '');
+    return longestEmail;
+}
+console.log(getLongestEmail(users));
+
+// 6 Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+function getListOfUsernamesAsString(arrParam) {
+    let userNameString = arrParam.reduce((user, person) => {
+        user += `\n${person.name}`; // You could just return the user here
+        return user;
+    }, "Your teachers are: ");
+    return userNameString;
+}
+console.log(getListOfUsernamesAsString(users));
+
+// BONUS Use .reduce to get the unique list of languages from the list of users.
+// function getUniqueListofLanguages(arrParam) {
+//
+// }
+
