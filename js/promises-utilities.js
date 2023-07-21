b04b1cdb0f543b416ecd13b30352911315362a89
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-21 15:53:11 -0500

    promises-async completed

--- js/promises-utilities.js
+++ js/promises-utilities.js
@@ -1,25 +1,61 @@
 "use strict";
 console.log(`hello from promises-utilities.js`);
 
+
+// ============================================================================================================
+// .then method
 // -----------------------------------------------------------------------------
 // GETTER FUNCTION
-export function getPerson(searchBar) {
-    const userId = searchBar.value;
-    const url = `https://api.github.com/users/${userId}/events`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'Authorization': GIT_HUB,
+// export function getPerson(searchBar) {
+//     const userId = searchBar.value;
+//     const url = `https://api.github.com/users/${userId}/events`;
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'Authorization': GIT_HUB,
+//         }
+//     }
+//     return fetch(url, options)
+//         .then(response => {
+//             return response.json()
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         })
+// }
+// -----------------------------------------------------------------------------
+// // RENDER FUNCTION
+// export function renderPersonInfo (events) {
+//     const pushEvents = events.filter(event => {
+//         return event.type === "PushEvent";
+//     })
+//     console.log(pushEvents);
+//     console.log(pushEvents[0].actor);
+//     console.log(`The date & time of your last commit was:` )
+//     console.log(`Your last commit =>`, pushEvents[0].payload.commits[0].message);
+// }
+// ============================================================================================================
+// promises-async
+// -----------------------------------------------------------------------------
+// GETTER FUNCTION
+export async function getPerson(searchBar) {
+    try {
+        const userId = searchBar.value;
+        const url = `https://api.github.com/users/${userId}/events`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'Authorization': GIT_HUB,
+            }
         }
+        const response = await fetch(url, options);
+        const events = await response.json();
+        return events;
+    } catch(error) {
+        console.log(error);
     }
-    return fetch(url, options)
-        .then(response => {
-            return response.json()
-        })
-        .catch(error => {
-            console.log(error);
-        })
 }
+
 // -----------------------------------------------------------------------------
 // RENDER FUNCTION
 export function renderPersonInfo (events) {
@@ -28,6 +64,8 @@ export function renderPersonInfo (events) {
     })
     console.log(pushEvents);
     console.log(pushEvents[0].actor);
-    console.log(`The date & time of your last commit was:` )
     console.log(`Your last commit =>`, pushEvents[0].payload.commits[0].message);
 }
+/*
+You do not need to async your render function!
+ */
--- js/promises.js
+++ js/promises.js
@@ -3,21 +3,34 @@ console.log(`hello from promises.js`);
 import {getPerson, renderPersonInfo} from './promises-utilities.js'
 
 
-(() => {
-    // Variables ...
+// (() => {
+//     // Variables ...
+//     const searchBar = document.querySelector('#search-input');
+
+//     // Event listeners ...
+//     searchBar.addEventListener('keyup' ,(e) => {
+//         if (e.keyCode === 13) {
+//             console.log(searchBar.value);
+//             getPerson(searchBar).then(events => {
+//                 renderPersonInfo(events);
+//             })
+//             searchBar.value = '';
+//         }
+//     })
+// })();
+// =====================================================================================================
+// promises- async
+(async () => {
+    // Variables...
     const searchBar = document.querySelector('#search-input');
 
-
-    // Event listeners ...
-    searchBar.addEventListener('keyup' ,(e) => {
+    // Event listeners...
+    searchBar.addEventListener('keyup' ,async(e) => {
         if (e.keyCode === 13) {
             console.log(searchBar.value);
-            getPerson(searchBar).then(events => {
-                renderPersonInfo(events);
-            })
+            const events = await getPerson(searchBar);
+            renderPersonInfo(events);
             searchBar.value = '';
         }
     })
-
-
 })();
