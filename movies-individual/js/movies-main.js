adb8b5e411ffcfef9b4d46b3d976806f95073452
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-24 09:10:59 -0500

    improved movies-main with map genre names

--- movies-individual/js/movies-main.js
+++ movies-individual/js/movies-main.js
@@ -74,72 +74,117 @@ function getMoviesBySearch (queryParam) {
             console.log(error);
         })
 }
+// This gives me access to all the movies.  It was done with the discover filter, and the read access token
+function discoverAllMovies() {
+    const urlDiscover = 'https://api.themoviedb.org/3/discover/movie';
+    const optionsDiscover = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${TMDB_API_READ_ACCESS_TOKEN}`
+        }
+    }
+
+    return fetch(urlDiscover, optionsDiscover)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+// This gives me access to all genres, genre id & genre name.  Turn this into a function? Declare it as a variable to use and map() corresponding genres?
+function genreList() {
+    const urlGenreList = 'https://api.themoviedb.org/3/genre/movie/list';
+    const optionsGenreList = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${TMDB_API_READ_ACCESS_TOKEN}`
+        }
+    }
+    return fetch(urlGenreList, optionsGenreList)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            return data;
+            console.log(data)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+
+const getGenreName = (genres, id) => {
+    // Find genre object in the genres array
+    let genre = genres.find(genre => genre.id === id);
+
+    // Check if a genre was found, if not return a default value
+    if (!genre) {
+        return "Genre not found";
+    }
+
+    // Return the genre's name
+    return genre.name;
+};
+
+
+// ---------------------------------------------------------------------------------------------------
 // IIFE...
 (() => {
     // Variables
     const searchBar = document.querySelector('#search-bar');
-
-    searchBar.addEventListener('keyup', (e) => {
-        if(e.keyCode === 13) {
-            console.log(searchBar.value);
-            getMoviesBySearch(searchBar.value).then(moviesData => {
-                searchBar.value = '';
-                console.log(`Fetch & then => `, moviesData);
-                const firstFilter = moviesData.results.filter((movie) => {
-                    return movie.original_language === 'en' && movie.backdrop_path !== null;
-                });
-                console.log(`firstFilter => `, firstFilter);
-            })
+    // Functions...
+    discoverAllMovies();
+    genreList().then(genres => {
+        // Events....
+        searchBar.addEventListener('keyup', (e) => {
+            if(e.keyCode === 13) {
+                console.log(searchBar.value);
+                getMoviesBySearch(searchBar.value).then(moviesData => {
+                    searchBar.value = '';
+                    console.log(`Fetch & then => `, moviesData);
+                    const firstFilter = moviesData.results.filter((movie) => {
+                        return movie.original_language === 'en' && movie.backdrop_path !== null;
+                    }).map((movie) => {
+                        return {
+                            ...movie,
+                            genre_names: movie.genre_ids.map(id => getGenreName(genres.genres, id))
+                        }
+                    });
+                    console.log(`firstFilter => `, firstFilter);
+                })
+            }
+        })
         }
-    })
+    )
+
+    // // Events....
+    // searchBar.addEventListener('keyup', (e) => {
+    //     if(e.keyCode === 13) {
+    //         console.log(searchBar.value);
+    //         getMoviesBySearch(searchBar.value).then(moviesData => {
+    //             searchBar.value = '';
+    //             console.log(`Fetch & then => `, moviesData);
+    //             const firstFilter = moviesData.results.filter((movie) => {
+    //                 return movie.original_language === 'en' && movie.backdrop_path !== null;
+    //             });
+    //             console.log(`firstFilter => `, firstFilter);
+    //         })
+    //     }
+    // })
+
+
 
 })();
 
-
 // ---------------------------------------------------------------------------------------------------
-// This gives me access to all the movies
-// It was done with the discover filter, and the read access token
-const urlDiscover = 'https://api.themoviedb.org/3/discover/movie';
-const optionsDiscover = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${TMDB_API_READ_ACCESS_TOKEN}`
-    }
-}
 
-fetch(urlDiscover, optionsDiscover)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data)
-    })
-    .catch(error => {
-        console.log(error);
-    })
-// ----------------------------------------------------------------------------------------------------
-
-// This gives me access to all genres, genre id & genre name
-// Turn this into a function? Declare it as a variable to use and map() corresponding genres?
-const urlGenreList = 'https://api.themoviedb.org/3/genre/movie/list';
-const optionsGenreList = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${TMDB_API_READ_ACCESS_TOKEN}`
-    }
-}
-fetch(urlGenreList, optionsGenreList)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data)
-    })
-    .catch(error => {
-        console.log(error);
-    })
 
 
 
