d5a82338b75c21c7feabbe9bf05d82c21d1051e5
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-25 17:02:25 -0500

    completed mexican wave challange, codewars

--- Codewars/Mexican-Wave/js/mexican-wave-main.js
+++ Codewars/Mexican-Wave/js/mexican-wave-main.js
@@ -3,3 +3,22 @@ console.log(`hello from mexican-wave-main.js`);
 
 // Look up
 // Mexican Wave on codewars.
+
+let string1 = `hello`;
+let string2 = ` gap `;
+
+function wave(str){
+    // Code here
+    let resultArray = [];
+    let capitalChar;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== ' ') { // Only proceed if the character is not a space
+            capitalChar = str.slice(i, i + 1).toUpperCase();
+            resultArray.push(str.substring(0, i) + capitalChar + str.substring(i + 1, str.length));
+        }
+    }
+    return resultArray;
+}
+
+console.log(wave(string1));
+console.log(wave(string2));
