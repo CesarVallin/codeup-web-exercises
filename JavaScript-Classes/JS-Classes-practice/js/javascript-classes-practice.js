99bd0bd65e0a60403bc5306556de3cac0d4bfca1
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-02 14:10:12 -0500

    working with JavaScript classes

---
+++ JavaScript-Classes/JS-Classes-practice/js/javascript-classes-practice.js
@@ -0,0 +1,25 @@
+
+
+class HelloTest {
+    static sayHello() {
+        return `hello there! (with static method!)`;
+    }
+}
+
+class HiTest {
+    sayHi() {
+        return `hi there, (with an instance of HiTest to access this method!)`;
+    }
+}
+
+(() => {
+    "use strict";
+
+    const message1 = HelloTest.sayHello();
+    console.log(message1);
+
+    const hiTestInstance = new HiTest();
+    const message2 = hiTestInstance.sayHi();
+    console.log(message2);
+
+}) ();
