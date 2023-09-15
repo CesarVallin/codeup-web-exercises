5f76cc802d9003bbb5bdd486be8a01044681bf8b
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-14 21:42:01 -0500

    notes on file-stack-practice

--- file-stack/file-stack-adlisterway/file-stack-practice.js
+++ file-stack/file-stack-adlisterway/file-stack-practice.js
@@ -42,7 +42,23 @@ import {FilePickerAPI} from "./file-stack-practice-utils.js";
             imageDisplay.innerHTML = `<p>Oops, no image</p>`;
         }
 
-    })
+    });
+
+    /**
+     *
+     * Certainly! The code you're asking about is using the FormData constructor to create a new FormData object. FormData is a built-in JavaScript object that allows you to easily capture and manipulate form data from HTML forms.
+     *
+     * Here's a breakdown of that part of the code:
+     *
+     * const formData = new FormData(form);: This line creates a new FormData object named formData. It takes one argument, which is the HTML form element (form) you want to capture data from. When you pass a form element to the FormData constructor, it automatically collects all the form input values within that form.
+     *
+     * const username = formData.get("username");: After creating the FormData object, you can use the get method to retrieve the values of specific form fields by their name attribute. In this case, it's retrieving the value of the input field with the name "username" and assigning it to the variable username.
+     *
+     * const email = formData.get("email");: Similarly, this line retrieves the value of the input field with the name "email" and assigns it to the variable email.
+     *
+     * So, the purpose of this code is to gather the values of the "username" and "email" input fields from the form and store them in the username and email variables, respectively. This makes it easy to access and work with the form data in JavaScript.
+     *
+     * */
 
 
 
