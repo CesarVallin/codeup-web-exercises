cfe78af7474ae592d1395b459b241236e0b68c53
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-17 16:34:45 -0500

    completed changes and modifications to Filestack in codeup-web-exercises

--- file-stack/file-stack-adlisterway/file-stack-practice-utils.js
+++ file-stack/file-stack-adlisterway/file-stack-practice-utils.js
@@ -5,12 +5,13 @@ export class FilePickerAPI {
     static async getFilePicker() {
         const client = await filestack.init(FILE_STACK_API);
         const options = {
-            fromSources: ["local_file_system", "url"],
+            fromSources: ["local_file_system"],
             transformations: {
-                crop: { aspectRatio: 1 / 1, force: true },
+                crop: { aspectRatio: 1 / 1, force: true},
+                circle: true,
             },
             imageMin: [200, 200],
-            imageMax: [200, 200],
+            imageMax: [400, 400],
             onFileUploadFinished: file => {
                 console.log(file.url);
                 const uploadPreview = document.querySelector("#upload-preview");
@@ -24,3 +25,24 @@ export class FilePickerAPI {
         client.picker(options).open();
     }
 }
+
+/** for upload preview, you can set the innerHTML here, or simply or use a set up like this:
+ *
+ *
+ * ON HTML
+ * <div style="text-align:center;">
+ *   <img id="result" style="width:600px" />
+ * </div>
+ *
+ *
+ * ON API call
+ * const tr = new FilestackTransform('API_KEY')
+ * tr.open(FILE_URL).then(res => { // replace FILE_URL with the link to the image
+ *   document.getElementById('result').src = res // display result of the transformations
+ * })
+ *
+ *
+ * NOTICE -------------------------
+ * DOM manipulation works with ().src, which is the image source attribute.
+ *
+ * */
