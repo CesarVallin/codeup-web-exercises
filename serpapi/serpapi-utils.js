0a27607e29486a7f6bdba8fe0d2b16a227139395
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-22 17:11:29 -0500

    serpapi trial

---
+++ serpapi/serpapi-utils.js
@@ -0,0 +1,11 @@
+
+export class SerpApi {
+
+    // static async getEventObjects() {
+    //
+    //     const url = "https://serpapi.com/search.json?engine=google_events&q=Events+in+Austin&hl=en&gl=us";
+    //     const optionsSearchResultsObject = {
+    //         method:
+    //     }
+    // }
+}
